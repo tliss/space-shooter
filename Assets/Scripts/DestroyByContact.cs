f9184614ec2354c1261b61aac02fcd5786186b9f
Taylor A. Liss <tayloraliss@gmail.com>
2019-02-13 10:21:06 -0500

    Can shoot asteroids

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        // Destroy laser bolt
+        Destroy(other.gameObject);
+        // Destroy gameObject this script is attached to (i.e. the asteroid)
+        Destroy(gameObject);
+    }
+}
