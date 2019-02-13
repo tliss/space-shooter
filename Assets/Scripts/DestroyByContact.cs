ec6b17c7d7b7875ddd6cc9e940c0f6e0af43f5ab
Taylor A. Liss <tayloraliss@gmail.com>
2019-02-13 10:31:50 -0500

    Add explosion effects

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
+        }
+
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
 
         // Destroy laser bolt
