25bae35eb230c30b9aeaffadae005287f355add6
Taylor Liss <tayloraliss@gmail.com>
2019-02-13 22:57:06 -0500

    Show score

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,21 @@ public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if(gameControllerObject == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,13 +29,14 @@ public class DestroyByContact : MonoBehaviour
             return;
         }
 
-
         Instantiate(explosion, transform.position, transform.rotation);
 
         if(other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
+
+        gameController.AddScore(scoreValue);
 
         // Destroy laser bolt
         Destroy(other.gameObject);
