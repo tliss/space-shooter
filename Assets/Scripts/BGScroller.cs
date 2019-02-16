efedd21a1a58a27d0a747848358b8e2d0dfab33e
Taylor A. Liss <tayloraliss@gmail.com>
2019-02-16 10:36:01 -0500

    Scroll the background

--- Assets/Scripts/BGScroller.cs
+++ Assets/Scripts/BGScroller.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BGScroller : MonoBehaviour
 {
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Time.time is the time (in seconds) since the start of the game
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        // move background along the Z axis x number of units, where x is newPosition.
+        transform.position = startPosition + Vector3.forward * newPosition;
     }
 }
